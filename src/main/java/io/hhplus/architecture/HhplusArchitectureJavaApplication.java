44608e37f256743b94e16b80bc912f6c8a6f68a4
jslee0826 <jslee0826@cj.net>
2024-12-24 15:39:26 +0900

    Initial commit

---
+++ src/main/java/io/hhplus/architecture/HhplusArchitectureJavaApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusArchitectureJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusArchitectureJavaApplication.class, args);
+    }
+
+}
